fdd0d1fcffc72f9c34d5ee5d4ff8a8e4b5a45ddc
Kayley Bannister <kayley.bannister@gmail.com>
2019-07-17 09:36:12 -0400

    created database.ref().push() and created a train object so that the four variables can be pushed to the database and it worked

---
+++ assets/train.js
@@ -0,0 +1,39 @@
+//FireBase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyA5kyYmg4Anz1XOpHeYMgHbIJNvRp7SZBg",
+    authDomain: "train-homework7-52115.firebaseapp.com",
+    databaseURL: "https://train-homework7-52115.firebaseio.com",
+    projectId: "train-homework7-52115",
+    storageBucket: "",
+    messagingSenderId: "278225043750",
+    appId: "1:278225043750:web:ffb9b4d38a27b8cb"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database();
+
+  var trainName = $("#train-input").val().trim();
+  var destination = $("#destination-input").val().trim();
+  var trainTime = $("#train-time-input").val().trim();
+  var frequency = $("#frequency-input").val().trim();
+
+  //creating object so that these can be pushed to the database?
+  var trainObject = {
+      trainName: trainName,
+      destination: destination,
+      trainTime: trainTime,
+      frequency: frequency
+    }
+
+  $("#submit-button").on("click", function(event){
+      event.preventDefault();
+
+    //testing to make sure the JS reads the user inputs
+    console.log("Submit Button has been Clicked");
+    console.log(trainObject);
+
+    //the below should push these to the database
+    database.ref().push(trainObject);
+
+  })
